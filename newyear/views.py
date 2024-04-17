fa04442eb3e2e54fd7840f6e7b9db7e9953b7903
Sai Tusa <ict.jomik@gmail.com>
2024-04-17 14:09:05 +1100

    completed the new year app

--- newyear/views.py
+++ newyear/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+now = datetime.now()
+
 
 def index(request):
-    return render(request,"newyear/index.html")
+    return render(request,"newyear/index.html",{"newyear": now.day ==1 and now.month == 1})
